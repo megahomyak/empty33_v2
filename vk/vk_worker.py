cb8d670d492e729322976409063f781bf5c65cf4
megahomyak <g.megahomyak@gmail.com>
2020-11-02 14:33:23 +0500

    Made VKWorker

---
+++ vk/vk_worker.py
@@ -0,0 +1,27 @@
+import random
+
+from simple_avk import SimpleAVK
+
+from vk import vk_constants
+
+
+class VKWorker(SimpleAVK):
+
+    async def reply(self, peer_id: int, text: str) -> None:
+        divided_text = (
+            text[i:i + vk_constants.SYMBOLS_LIMIT]
+            for i in range(
+                0,
+                len(text),
+                vk_constants.SYMBOLS_LIMIT
+            )
+        )
+        for part in divided_text:
+            await self.call_method(
+                "messages.send",
+                {
+                    "text": part,
+                    "peer_id": peer_id,
+                    "random_id": random.randint(-1_000_000, 1_000_000)
+                }
+            )
