bef4dc054eba38436b911e407caa802e8f638693
megahomyak <g.megahomyak@gmail.com>
2020-11-02 16:21:42 +0500

    Now there are a file for secret constants, which is not in git, and file for public constants, which is in git

--- vk/vk_constants.py
+++ vk/vk_constants.py
@@ -1,20 +1,18 @@
 from configparser import ConfigParser
 
-
 _config_parser = ConfigParser()
-_config_parser.read("vk/vk_constants.ini", encoding="utf-8")
+_config_parser.read("vk/vk_secret_constants.ini", encoding="utf-8")
 
 
 _secrets_sector = _config_parser["SECRETS"]
-
 TOKEN = _secrets_sector["token"]
-GROUP_ID = int(_secrets_sector["group_id"])
 
 
 _public_sector = _config_parser["PUBLIC"]
-
+GROUP_ID = int(_secrets_sector["group_id"])
 TAG = _public_sector["tag"]
 SYMBOLS_LIMIT = int(_public_sector["symbols_limit"])
+
 
 with open("simple_replies.txt", "r", encoding="utf-8") as f:
     SIMPLE_REPLIES = list(filter(None, f.read().split("\n")))
