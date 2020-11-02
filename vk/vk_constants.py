b1f140209264d659d74ce806974fc81a54805df9
megahomyak <g.megahomyak@gmail.com>
2020-11-02 21:46:25 +0500

    Added one more ConfigParser object for second .ini file, so the bug is fixed

--- vk/vk_constants.py
+++ vk/vk_constants.py
@@ -1,15 +1,18 @@
 from configparser import ConfigParser
 
-_config_parser = ConfigParser()
-_config_parser.read("vk/vk_secret_constants.ini", encoding="utf-8")
+_secrets_parser = ConfigParser()
+_secrets_parser.read("vk/vk_secret_constants.ini", encoding="utf-8")
+
+_public_parser = ConfigParser()
+_public_parser.read("vk/vk_public_constants.ini", encoding="utf-8")
 
 
-_secrets_sector = _config_parser["SECRETS"]
+_secrets_sector = _secrets_parser["SECRETS"]
 TOKEN = _secrets_sector["token"]
 
 
-_public_sector = _config_parser["PUBLIC"]
-GROUP_ID = int(_secrets_sector["group_id"])
+_public_sector = _public_parser["PUBLIC"]
+GROUP_ID = int(_public_sector["group_id"])
 TAG = _public_sector["tag"]
 SYMBOLS_LIMIT = int(_public_sector["symbols_limit"])
 CHAT_ERROR_MESSAGE = _public_sector["chat_error_message"]
