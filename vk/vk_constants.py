3ac2de5d308045321d16dd690e48021d9bb01936
megahomyak <g.megahomyak@gmail.com>
2020-11-02 14:22:21 +0500

    Added variables to access constants in vk_constants.py

---
+++ vk/vk_constants.py
@@ -0,0 +1,17 @@
+from configparser import ConfigParser
+
+
+_config_parser = ConfigParser()
+_config_parser.read("vk/vk_constants.ini")
+
+
+_secrets_sector = _config_parser["SECRETS"]
+
+TOKEN = _secrets_sector["token"]
+GROUP_ID = int(_secrets_sector["group_id"])
+
+
+_public_sector = _config_parser["PUBLIC"]
+
+TAG = _public_sector["tag"]
+SYMBOLS_LIMIT = int(_public_sector["symbols_limit"])
